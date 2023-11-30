a7cc4fc77112ec81e5718e651a338cad8b6c116c
HongChan Yoon <hongchani0324@gmail.com>
2023-12-01 08:23:33 +0900

    Feat: User 테이블에 생성일, 수정일, 생성자, 수정자를 상속받아 자동적으로  테이블에 정보가 저장될 수 있도록 기능 추가
    1. BaseEntity 추상 클래스를 추가하여 EventListener를 활용해 상속받는 Entity에 변경이 일어날 경우 자동적으로 데이터가 같이 저장될 수 있도록 함.
    2. LoginUserAuditorAware 클래스를 구현하여 권한이 없는 사용자일 경우와, 권한을 가지고 있는 사용자의 경우로 나누어 User 엔티티 수정자가 자동적으로 수정될 수 있도록 함.
    3. Controller에 불필요한 정보 삭제.
    4. BaseEntity 사용을 위한 SecurityConfig에 lombok 추가

--- src/main/java/com/cos/security1/config/auth/PrincipalDetailsService.java
+++ src/main/java/com/cos/security1/config/auth/PrincipalDetailsService.java
@@ -3,6 +3,7 @@ package com.cos.security1.config.auth;
 import java.util.Optional;
 import java.util.regex.Pattern;
 
+import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
 
 import org.springframework.security.core.userdetails.UserDetails;
@@ -22,51 +23,49 @@ import lombok.RequiredArgsConstructor;
 // /login 요청이 오면 자동으로 UserDetailsService타입으로 IoC되어있는 loadUserByUsername 메서드가 실행되고
 // 해당 객체 (UserDetailsService) 타입을 가진 객체를 찾아 메소드를 적용시킴.
 @RequiredArgsConstructor
-@Service	// IoC로 등록
-public class PrincipalDetailsService implements UserDetailsService{
+@Service    // IoC로 등록
+public class PrincipalDetailsService implements UserDetailsService {
 
-	private final UserRepository userRepository;
-	private final NotSignedUserRepository notSignedUserRepository;
-	
-	private BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
-	
-	// 받는 파라미터를 바꿀 시에 SecurityConfig에서 .usernameParameter("프론트에서 name으로 설정한 이름")에 설정해준다.
-	// security session(내부 Authentication(내부 UserDetails)) 처리를 loadUserByUsername이 알아서 처리해줌.
-	// 함수 종료시 @AuthenticationPrincipal annotation이 만들어진다.
-	@Override
-	public UserDetails loadUserByUsername(String userId) throws UsernameNotFoundException {
-		User userEntity = userRepository.findByUserId(userId);
-		if (userEntity != null) {
-			return new PrincipalDetails(userEntity);
-		}
-		
-		return null;
-	}
-	
-	@Transactional
+    private final UserRepository userRepository;
+    private final NotSignedUserRepository notSignedUserRepository;
+    private final EntityManager em;
+
+    private BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+
+    // 받는 파라미터를 바꿀 시에 SecurityConfig에서 .usernameParameter("프론트에서 name으로 설정한 이름")에 설정해준다.
+    // security session(내부 Authentication(내부 UserDetails)) 처리를 loadUserByUsername이 알아서 처리해줌.
+    // 함수 종료시 @AuthenticationPrincipal annotation이 만들어진다.
+    @Override
+    public UserDetails loadUserByUsername(String userId) throws UsernameNotFoundException {
+        User userEntity = userRepository.findByUserId(userId);
+        if (userEntity != null) {
+            return new PrincipalDetails(userEntity);
+        }
+
+        return null;
+    }
+
+    @Transactional
     public void joinUser(User user) {
-        System.out.println("user.getUserId() = " + user.getUserId());
-        System.out.println("user.getPassword() = " + user.getPassword());
-        System.out.println("user = " + user.getUserEmail());
 
         if (user.getUserId() != null && user.getPassword() != null && user.getUserEmail() != null) {
-            
+
             User userId = userRepository.findByUserId(user.getUserId());
             User email = userRepository.findByUserEmail(user.getUserEmail());
-            
+
             if (userId == null && email == null) {
                 user.setRole("ROLE_USER");
                 String rawPassword = user.getPassword();
                 String encPassword = bCryptPasswordEncoder.encode(rawPassword);
-                
+
                 user.setPassword(encPassword);
                 userRepository.save(user);
 
             }
-        
+
         }
     }
-    
+
     public Boolean checkExistUserId(String userId) {
 
         int isUsing = 0;
@@ -81,7 +80,7 @@ public class PrincipalDetailsService implements UserDetailsService{
 
         return false;
     }
-    
+
     public Boolean checkExistUserEmail(String userEmail) {
 
         int isUsing = 0;
@@ -96,143 +95,142 @@ public class PrincipalDetailsService implements UserDetailsService{
 
         return false;
     }
-    
+
 
     public boolean findUserId(User user) {
-        
+
         boolean result = false;
-        
+
         if (user.getUserId() != null) {
             User userName = userRepository.findByUserId(user.getUserId());
-            
+
             if (userName != null) {
-               result = true;
+                result = true;
             }
         }
-        
+
         return result;
     }
-    
+
     public boolean findUserEmail(User user) {
-        
+
         boolean result = false;
-        
-           if (user.getUserEmail() != null) {
-                User email = userRepository.findByUserEmail(user.getUserEmail());
-                
-                if (email != null) {
-                    result = true;
-                }
+
+        if (user.getUserEmail() != null) {
+            User email = userRepository.findByUserEmail(user.getUserEmail());
+
+            if (email != null) {
+                result = true;
             }
-           return result;
+        }
+        return result;
     }
-    
+
     public String findUserIdByUserEmailAndUserName(User user) {
-        
+
         String userEmail = user.getUserEmail();
         String userName = user.getUserName();
         String result = "";
-        
+
         if (userEmail != null & userName != null) {
             User userId = userRepository.findByUserNameAndUserEmail(user.getUserName(), user.getUserEmail());
-            
+
             if (userId != null) {
-                result = userId.getUserId(); 
+                result = userId.getUserId();
             }
         }
         return result;
     }
-    
+
     // 로그인 후 비밀번호를 변경할 method
     @Transactional
     public String updateLoginedUserPassword(User user, PrincipalDetails principalDetails) {
-        
+
         String result = "false";
         String userId = principalDetails.getUser().getUserId();
         String newPassword = user.getPassword();
-        System.out.println(newPassword);
         Pattern pattern = Pattern.compile("[ !@#$%^&*(),.?\":{}|<>]");
-        
-        if (newPassword != null && newPassword.length() >= 8 && pattern.matcher(newPassword).find() == true) {
-            Optional<User> getUser = Optional.of(userRepository.findByUserId(userId));
+
+        if (newPassword != null && newPassword.length() >= 8 && pattern.matcher(newPassword).find()) {
+            User getUser = userRepository.findByUserId(userId);
             String encPassword = bCryptPasswordEncoder.encode(newPassword);
-            
-            getUser.ifPresent(thisUser -> {
-                thisUser.setPassword(encPassword);
-                userRepository.save(thisUser);
-            });
+
+            System.out.println("+++++++++++++++++");
+            getUser.changePassword(encPassword);
+            System.out.println("=================");
+
             result = "true";
         }
-            return result;
+        return result;
     }
-    
+
     // 로그인시 비밀번호 모를 때 비밀번호 update method
     @Transactional
     public String updateUnloginedUserPassword(User user) {
-        
+
         String result = "false";
         String userId = user.getUserId();
         String userName = userRepository.findUserNameByUserIdAndUserEmail(user.getUserId(), user.getUserEmail()).getUserName();
         String userEmail = user.getUserEmail();
         String newPassword = user.getPassword();
         Pattern pattern = Pattern.compile("[ !@#$%^&*(),.?\":{}|<>]");
-        
+
         if (userId != null && userName != null && userEmail != null && newPassword != null) {
-            
+
             if (newPassword != null && newPassword.length() >= 8 && pattern.matcher(newPassword).find() == true) {
                 Optional<User> getUser = userRepository.findByUserIdAndUserEmailAndUserName(userId, userEmail, userName);
-                
+
                 if (getUser != null) {
                     String encPassword = bCryptPasswordEncoder.encode(newPassword);
-                    
+
                     getUser.ifPresent(thisUser -> {
                         thisUser.setPassword(encPassword);
-                        
+
                         userRepository.save(thisUser);
                     });
-                    
+
                     result = "true";
-                }else {
+                } else {
                     result = "id또는 이름 또는 이메일을 확인해주세요";
                 }
-            }else {
+            } else {
                 result = "비밀번호 설정 조건을 확인해 주세요";
             }
-        }else {
+        } else {
             result = "누락된 정보가 없는지 확인해주세요!";
         }
-        
+
         return result;
     }
-    
+
     public int checkLimitCount(NotSignedUser notSignedUser) {
-        
+
         NotSignedUser usedCount = notSignedUserRepository.findUsedCountByVisitUserIp(notSignedUser.getVisitUserIp());
-        
-        if(usedCount == null) {
+
+        if (usedCount == null) {
             return 0;
-        }else if (usedCount.getUsedCount() < 3) {
+        } else if (usedCount.getUsedCount() < 3) {
             return usedCount.getUsedCount();
         }
-        
+
         return 500;
     }
-    
+
     public boolean manageVisitingUser(NotSignedUser notSignedUser) {
         int totalCount = checkLimitCount(notSignedUser);
-        
+
         // IP 존재 O & 사용 횟수 0 -> save
-        if(totalCount == 0) {
-            notSignedUserRepository.save(notSignedUser);            
+        if (totalCount == 0) {
+            notSignedUserRepository.save(notSignedUser);
             return true;
-            
+
             // IP 존재 O & 사용 횟수 1 이상 -> update
-        }else if(totalCount != 500) {
+        } else if (totalCount != 500) {
             Optional<NotSignedUser> noUser = notSignedUserRepository.findByVisitUserIp(notSignedUser.getVisitUserIp());
-            
+
             noUser.ifPresent(thisUser -> {
                 thisUser.setUsedCount(totalCount + 1);
-                
+
                 notSignedUserRepository.save(thisUser);
             });
             return true;
@@ -240,5 +238,5 @@ public class PrincipalDetailsService implements UserDetailsService{
         // IP 존재 O & 사용 횟수 3 -> return false;
         return false;
     }
-    
+
 }
